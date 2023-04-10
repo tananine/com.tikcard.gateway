c18dbea0fe5f9e7bef0398601de912acc0363161
tananine <tana9dev@gmail.com>
2023-04-10 14:04:19 +0700

    add env key and refactor code

--- plugins/auth/manifest.js
+++ plugins/auth/manifest.js
@@ -1,8 +1,11 @@
 module.exports = {
-  version: '1.2.0',
+  version: "1.2.0",
   init: function (pluginContext) {
-    let policy = require('./policies/authen')
-    pluginContext.registerPolicy(policy)
+    let policy = require("./policies/authen");
+    pluginContext.registerPolicy(policy);
   },
-  policies: ['authen'],
-}
+  policies: ["authen"],
+  schema: {
+    $id: "http://express-gateway.io/schemas/plugin/auth.json",
+  },
+};
--- plugins/auth/policies/authen.js
+++ plugins/auth/policies/authen.js
@@ -1,37 +1,46 @@
-const axios = require('axios')
-const jwt = require('jsonwebtoken')
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-  name: 'authen',
+  name: "authen",
+  schema: {
+    $id: "http://express-gateway.io/schemas/policies/authen.json",
+    type: "object",
+    properties: {
+      secretKey: {
+        type: "string",
+      },
+    },
+  },
   policy: (actionParams) => {
     return (req, res, next) => {
-      const authHeader = req.get('Authorization')
-      let token
+      const authHeader = req.get("Authorization");
+      let token;
       try {
-        token = authHeader.split(' ')[1]
+        token = authHeader.split(" ")[1];
       } catch {
-        return res.status(401).json({ message: 'ไม่สามารถแยก Token ได้' })
+        return res.status(401).json({ message: "ไม่สามารถแยก Token ได้" });
       }
       axios
-        .get('http://docker.for.mac.localhost:3001/auth/authentication', {
+        .get(`${process.env.SERVICE_AUTH_HOST}/auth/authentication`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         })
         .then((account) => {
           if (!account) {
-            return res.status(401).json({ message: 'ไม่พบ Account' })
+            return res.status(401).json({ message: "ไม่พบ Account" });
           }
-          const secretKey = 'secret'
-          const jsonToken = jwt.sign(account.data, secretKey)
-          req.headers.token = `Bearer ${jsonToken}`
-          next()
+          const secretKey = actionParams.secretKey;
+          const jsonToken = jwt.sign(account.data, secretKey);
+          req.headers.token = `Bearer ${jsonToken}`;
+          next();
         })
         .catch((error) => {
           return res
             .status(401)
-            .json({ message: 'authen ไม่สำเร็จ', error: error })
-        })
-    }
+            .json({ message: "authen ไม่สำเร็จ", error: error });
+        });
+    };
   },
-}
+};
